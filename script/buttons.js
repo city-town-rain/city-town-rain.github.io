3350d3f333bddee92e74abb9701408be36fdbc8f
citytownrain <ccy741852963@outlook.com>
2024-09-25 23:29:42 +0800

    commit

--- script/buttons.js
+++ script/buttons.js
@@ -2,11 +2,11 @@ var buttons = document.getElementById('menuButton');
 var menus = document.getElementById('menu'); 
 var divMenu = document.getElementById('headMenu');
 buttons.addEventListener('click', function(event) {
-menus.style.display = menus.style.display === 'block' ? 'none' : 'block';
-    if(menus.style.display == "block" ){
-        divMenu.style.height = parseInt(divMenu.offsetHeight)+300+"px";                  
-    }
-    else{
-        divMenu.style.height = "60px";
-    }                
-});
+    menus.style.display = menus.style.display === 'block' ? 'none' : 'block';
+        if(menus.style.display == "block" ){
+            divMenu.style.height = parseInt(divMenu.offsetHeight)+300+"px";                  
+        }
+        else{
+            divMenu.style.height = "60px";
+        }                
+    });
